f2b4bb92428949fbda1a8bb0854fd60a21a087b2
filip_somljacan <filip.somljacan@gmail.com>
2025-11-02 10:48:01 +0100

    Added cypress real events for hovering

--- cypress/e2e/uploadDeletePhoto.cy.js
+++ cypress/e2e/uploadDeletePhoto.cy.js
@@ -15,19 +15,17 @@ describe("Photo Upload Functionality", () => {
 
     cy.get("button").contains("Upload").click();
     cy.wait(5000); // wait for upload to complete
-  }),
-    it("should delete PhotoTest.jpg from gallery", () => {
-      cy.visit(
-        "https://demo.baasic.com/angular/starterkit-photo-gallery/album/detail/zmR8TYeHhgNNoLd408Qix1"
-      );
-      // Delete uploaded photo
-      cy.get('.thumbnail__img[style*="PhotoTest.jpg"]', { timeout: 10000 })
-        .should("exist")
-        .parents(".thumbnail")
-        .find(".thumbnail__info__delete")
-        .click();
 
-      // Verify it’s gone
-      cy.get('.thumbnail__img[style*="PhotoTest.jpg"]').should("not.exist");
-    });
+    // Hover over the thumbnail to reveal the delete button
+    cy.get('.thumbnail__img[style*="PhotoTest.jpg"]', { timeout: 10000 })
+      .should("exist")
+      .trigger("mouseover");
+    cy.wait(400); // wait for hover effect
+
+    // Click the delete button (visible after hover)
+    cy.get(".thumbnail__info__delete").should("be.visible").click();
+
+    // Verify it’s gone
+    cy.get('.thumbnail__img[style*="PhotoTest.jpg"]').should("not.exist");
+  });
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,6 +14,8 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import "./commands";
 
-import 'cypress-file-upload';
+import "cypress-file-upload";
+
+import "cypress-real-events/support";
